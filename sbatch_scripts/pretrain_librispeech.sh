26b2d499e516322d35c501b011c537ea4c9d8c66
lemswasabi <leminhnguyen@outlook.com>
2021-02-28 20:38:36 +0100

    train own small libri model

--- sbatch_scripts/pretrain_librispeech.sh
+++ sbatch_scripts/pretrain_librispeech.sh
@@ -2,19 +2,20 @@
 #SBATCH -J prelib
 #SBATCH --mail-type=begin,fail
 #SBATCH --mail-user=le.nguyen.001@student.uni.lu
-#SBATCH -N 4
-#SBATCH --ntasks-per-node=28
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-task=1
 #SBATCH -C volta32
-#SBATCH --time=2-00:00:00
+#SBATCH --time=0-00:20:00
 #SBATCH -p gpu
 #SBATCH --qos=normal
-#SBATCH --output='slurm_outputs'
+#SBATCH --output='slurm_outputs/pretrain_libri_base_%A.out'
 
 echo "== Starting run at $(date)"
 echo "== Job ID: ${SLURM_JOBID}"
 echo "== Node list: ${SLURM_NODELIST}"
 echo "== Submit dir. : ${SLURM_SUBMIT_DIR}"
 
-source .venv/bin/activate
-python3 main.py --config config/libri/las_460_fixed_character.yaml --logdir las_460_fixed_character_logdir --name las_460_fixed_character
+source .bsp06/bin/activate
+HYDRA_FULL_ERROR=1
+fairseq-hydra-train --config-dir librispeech/config/pretraining --config-name wav2vec2_base_librispeech
